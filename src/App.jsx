eb9bdc52003406a9be7697e865a910d2f0c940c5
Mohit Yadav <my701319@gmail.com>
2023-11-18 18:27:25 +0530

    nineth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home";
+
 function App() {
   return (
     <>
       <div className="w-full h-[100dvh] flex justify-center items-center bg-[#242424] text-[whitesmoke] ">
         Expenser
+        <Home />
       </div>
     </>
   );
