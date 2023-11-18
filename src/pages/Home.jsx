1f4d59cc82076b1a10699613bcd807d36327e7ef
Mohit Yadav <my701319@gmail.com>
2023-11-18 21:09:21 +0530

    eleventh commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,11 @@ import Header from "../components/Header";
 function Home() {
   return (
     <>
-      <Header />
-      <div className="w-full h-[100dvh] flex justify-center items-center bg-[#242424] text-[whitesmoke]">
-        Home
+      <div className="w-full h-[100dvh] flex flex-col">
+        <Header />
+        <div className="grow flex justify-center items-center bg-[#242424] text-[whitesmoke]">
+          Home
+        </div>
       </div>
     </>
   );
