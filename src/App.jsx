5c2d8fc2fafb5b4c00582651ce5d4211ee4fa5b4
Mohit Yadav <my701319@gmail.com>
2023-11-18 14:45:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="w-full h-[100dvh] bg-[#242424] flex justify-center items-center text-[whitesmoke]">
+        Mahadev
+      </div>
+    </>
+  );
+}
+
+export default App;
