071a6773c49aff443a0febbad6a7690a79af746a
Mohit Yadav <my701319@gmail.com>
2023-11-18 21:17:05 +0530

    twelveth commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 function Header() {
   return (
-    <div className="w-full flex justify-center bg-[#202020] text-[whitesmoke] py-2 text-2xl">
-      Header
+    <div className="w-full flex bg-[#202020] text-[whitesmoke] px-4 lg:px-12 py-3 text-3xl">
+      Xpenser
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import Header from "../components/Header";
 
 function Home() {
   return (
     <>
-      <div className="w-full h-[100dvh] flex flex-col">
+      <div className="w-full h-[100dvh] flex flex-col text-xl">
         <Header />
         <div className="grow flex justify-center items-center bg-[#242424] text-[whitesmoke]">
           Home
