76bf6921fc1eb66c18e5ef1175d292e8e10459cd
Mohit Yadav <my701319@gmail.com>
2023-11-19 12:05:27 +0530

    seventeenth commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="w-full flex items-center justify-between bg-[#202020] text-[whitesmoke] px-4 lg:px-12">
       <div className="text-3xl py-3">Xpenser</div>
-      <div className={clicked? "fixed top-16 right-0 flex flex-col items-center space-y-2 bg-[#242424] p-3 rounded md:space-x-0 md:space-y-2 md:flex-row md:bg-inherit md:static":"hidden md:flex md:items-center md:space-x-2"}>
+      <div className={clicked? "fixed top-16 right-0 flex flex-col items-center space-y-2 bg-[#242424] p-3 rounded-lg md:space-x-0 md:space-y-2 md:flex-row md:bg-inherit md:static":"hidden md:flex md:items-center md:space-x-2"}>
         <div className="w-full px-4 py-2 rounded-lg flex justify-center active:bg-[#393939] active:text-gray-400 md:w-fit sm:hover:bg-[#393939] sm:active:bg-[#323232] sm:hover:text-gray-400 sm:cursor-pointer">
           Login
         </div>
