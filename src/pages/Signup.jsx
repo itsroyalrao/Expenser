940c43cc1e8a6edb6ad941de71ba9e219eb40ad5
Mohit Yadav <my701319@gmail.com>
2023-11-19 18:34:46 +0530

    thirty-first commit

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-function Login() {
+function Signup() {
   return (
     <div className="w-full h-[100dvh] flex items-center justify-center bg-[#202020]">
       <div className="w-full h-full flex flex-col items-center justify-center bg-white py-[10%] space-y-6">
@@ -9,37 +9,42 @@ function Login() {
         </div>
         <div className="w-full flex flex-col items-center">
           <div className="w-[80%] flex flex-col space-y-4">
-            <div>
+            <div className="space-y-2">
+              <input
+                type="text"
+                className="w-full px-3 py-2 border-2 rounded-lg"
+                placeholder="Username"
+              />
               <input
                 type="email"
                 className="w-full px-3 py-2 border-2 rounded-lg"
                 placeholder="Email address"
               />
-              <div className="flex justify-end px-1 text-blue-500 text-lg">
-                Forgot password?
-              </div>
               <input
                 type="password"
                 className="w-full px-3 py-2 border-2 rounded-lg"
                 placeholder="Password"
               />
+              <input
+                type="password"
+                className="w-full px-3 py-2 border-2 rounded-lg"
+                placeholder="Confirm password"
+              />
             </div>
             <div className="w-full bg-blue-600 flex justify-center py-2 rounded-lg text-white text-xl">
-              Login
+              Sign up
             </div>
             <div className="text-lg flex justify-center space-x-1">
-              <span>Don&apos;t you have an account?</span>
-              <Link to={"/signup"} className="font-bold text-green-600">
-                Signup
-              </Link>
+              <span>Already have an account?</span>
+              <Link to={'/login'} className="font-bold text-green-600">Login </Link>
             </div>
           </div>
         </div>
 
         <hr className="w-[90%] border-t-2 border-gray-300" />
 
-        <div className="w-full flex flex-col items-center space-y-4">
-          <div className="text-xl">Or continue with</div>
+        <div className="w-full flex flex-col items-center space-y-3">
+          <div className="text-xl">Or join with</div>
           <div className="flex justify-center w-[80%] bg-red-500 py-2 text-[whitesmoke] text-xl rounded-lg">
             Google
           </div>
@@ -55,4 +60,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Signup;
