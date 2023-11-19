875fce719f1a2bf8889433897d462e9881d6c136
Mohit Yadav <my701319@gmail.com>
2023-11-19 17:07:23 +0530

    twenty-sixth commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,19 @@
 function Login() {
   return (
     <div className="w-full h-[100dvh] flex items-center justify-center bg-[#202020]">
-      <div className="w-full h-full flex flex-col items-center bg-white py-[5%] space-y-4">
+      <div className="w-full h-full flex flex-col items-center bg-white py-[10%] space-y-6">
         <div className="flex justify-center text-4xl font-bold text-blue-600">
           Expenser
         </div>
-        <div className="w-[80%] flex flex-col space-y-1">
+        <div className="w-[80%] flex flex-col">
           <input type="email" className="w-full px-3 py-2 border-2 rounded-lg" placeholder="Email address" />
+          <div className="flex justify-end px-1 text-blue-400">Forgot password?</div>
           <input type="email" className="w-full px-3 py-2 border-2 rounded-lg" placeholder="Password" />
-          <div className="px-1 py-1 text-blue-400">Forgot password?</div>
-          <div className="w-full bg-blue-600 flex justify-center py-2 rounded-lg text-white">Login</div>
+          <div className="w-full bg-blue-600 flex justify-center py-2 rounded-lg text-white mt-4">Login</div>
         </div>
+
+        <hr className="w-[90%] border-t-2 border-gray-300" />
+
       </div>
     </div>
   );
