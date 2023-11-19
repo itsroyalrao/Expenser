1b19d4c4b75652ec80b61db70439c2503663c54e
Mohit Yadav <my701319@gmail.com>
2023-11-19 18:02:04 +0530

    twenty-nineth commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Login() {
   return (
     <div className="w-full h-[100dvh] flex items-center justify-center bg-[#202020]">
@@ -6,19 +8,41 @@ function Login() {
           Expenser
         </div>
         <div className="w-[80%] flex flex-col">
-          <input type="email" className="w-full px-3 py-2 border-2 rounded-lg" placeholder="Email address" />
-          <div className="flex justify-end px-1 text-blue-400">Forgot password?</div>
-          <input type="email" className="w-full px-3 py-2 border-2 rounded-lg" placeholder="Password" />
-          <div className="w-full bg-blue-600 flex justify-center py-2 rounded-lg text-white text-xl mt-4">Login</div>
+          <input
+            type="email"
+            className="w-full px-3 py-2 border-2 rounded-lg"
+            placeholder="Email address"
+          />
+          <div className="flex justify-end px-1 text-blue-400">
+            Forgot password?
+          </div>
+          <input
+            type="email"
+            className="w-full px-3 py-2 border-2 rounded-lg"
+            placeholder="Password"
+          />
+          <div className="w-full bg-blue-600 flex justify-center py-2 rounded-lg text-white text-xl mt-4">
+            Login
+          </div>
+        </div>
+
+        <div className="text-lg">
+          Don&apos;t you have an account? <Link className="font-bold text-green-600">Signup </Link>
         </div>
 
         <hr className="w-[90%] border-t-2 border-gray-300" />
 
-        <div className="py-2">Or Login with</div>
+        <div className="text-xl">Or Login with</div>
 
-        <div className="flex justify-center w-[80%] bg-red-500 py-2 text-[whitesmoke] text-xl rounded-lg">Google</div>
-        <div className="flex justify-center w-[80%] bg-blue-500 py-2 text-[whitesmoke] text-xl rounded-lg">Facebook</div>
-        <div className="flex justify-center w-[80%] bg-black py-2 text-[whitesmoke] text-xl rounded-lg">Github</div>
+        <div className="flex justify-center w-[80%] bg-red-500 py-2 text-[whitesmoke] text-xl rounded-lg">
+          Google
+        </div>
+        <div className="flex justify-center w-[80%] bg-blue-500 py-2 text-[whitesmoke] text-xl rounded-lg">
+          Facebook
+        </div>
+        <div className="flex justify-center w-[80%] bg-black py-2 text-[whitesmoke] text-xl rounded-lg">
+          Github
+        </div>
       </div>
     </div>
   );
