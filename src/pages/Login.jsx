eb752085f5b65d5fb84fc5f81189b0185e9f55f9
Mohit Yadav <my701319@gmail.com>
2023-11-19 20:35:31 +0530

    thirty-second commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,51 +3,53 @@ import { Link } from "react-router-dom";
 function Login() {
   return (
     <div className="w-full h-[100dvh] flex items-center justify-center bg-[#202020]">
-      <div className="w-full h-full flex flex-col items-center justify-center bg-white py-[10%] space-y-6">
+      <div className="w-full sm:w-[80%] lg:w-[60%] h-full sm:h-[80%] flex flex-col items-center justify-center bg-white py-[10%] space-y-6 sm:space-y-12 rounded-2xl">
         <div className="flex justify-center text-4xl font-bold text-blue-600">
           Expenser
         </div>
-        <div className="w-full flex flex-col items-center">
-          <div className="w-[80%] flex flex-col space-y-4">
-            <div>
-              <input
-                type="email"
-                className="w-full px-3 py-2 border-2 rounded-lg"
-                placeholder="Email address"
-              />
-              <div className="flex justify-end px-1 text-blue-500 text-lg">
-                Forgot password?
+        <div className="w-full flex flex-col items-center sm:flex-row">
+          <div className="w-full flex flex-col items-center pb-4">
+            <div className="w-[80%] flex flex-col space-y-4">
+              <div>
+                <input
+                  type="email"
+                  className="w-full px-3 py-2 border-2 rounded-lg"
+                  placeholder="Email address"
+                />
+                <div className="flex justify-end px-1 text-blue-500 text-lg">
+                  Forgot password?
+                </div>
+                <input
+                  type="password"
+                  className="w-full px-3 py-2 border-2 rounded-lg"
+                  placeholder="Password"
+                />
               </div>
-              <input
-                type="password"
-                className="w-full px-3 py-2 border-2 rounded-lg"
-                placeholder="Password"
-              />
-            </div>
-            <div className="w-full bg-blue-600 flex justify-center py-2 rounded-lg text-white text-xl">
-              Login
-            </div>
-            <div className="text-lg flex justify-center space-x-1">
-              <span>Don&apos;t you have an account?</span>
-              <Link to={"/signup"} className="font-bold text-green-600">
-                Signup
-              </Link>
+              <div className="w-full bg-blue-600 flex justify-center py-2 rounded-lg text-white text-xl">
+                Login
+              </div>
+              <div className="text-lg flex justify-center space-x-1">
+                <span>Don&apos;t you have an account?</span>
+                <Link to={"/signup"} className="font-bold text-green-600">
+                  Signup
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
 
-        <hr className="w-[90%] border-t-2 border-gray-300" />
+          <div className="w-[90%] h-1 sm:w-1 sm:h-full bg-gray-500 rounded-full"></div>
 
-        <div className="w-full flex flex-col items-center space-y-4">
-          <div className="text-xl">Or continue with</div>
-          <div className="flex justify-center w-[80%] bg-red-500 py-2 text-[whitesmoke] text-xl rounded-lg">
-            Google
-          </div>
-          <div className="flex justify-center w-[80%] bg-blue-500 py-2 text-[whitesmoke] text-xl rounded-lg">
-            Facebook
-          </div>
-          <div className="flex justify-center w-[80%] bg-black py-2 text-[whitesmoke] text-xl rounded-lg">
-            Github
+          <div className="w-full flex flex-col items-center space-y-4 mt-4 sm:mt-0">
+            <div className="text-xl">Or continue with</div>
+            <div className="flex justify-center w-[80%] bg-red-500 py-2 text-[whitesmoke] text-xl rounded-lg">
+              Google
+            </div>
+            <div className="flex justify-center w-[80%] bg-blue-500 py-2 text-[whitesmoke] text-xl rounded-lg">
+              Facebook
+            </div>
+            <div className="flex justify-center w-[80%] bg-black py-2 text-[whitesmoke] text-xl rounded-lg">
+              Github
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,56 +3,60 @@ import { Link } from "react-router-dom";
 function Signup() {
   return (
     <div className="w-full h-[100dvh] flex items-center justify-center bg-[#202020]">
-      <div className="w-full h-full flex flex-col items-center justify-center bg-white py-[10%] space-y-6">
+      <div className="w-full sm:w-[80%] lg:w-[60%] h-full sm:h-[80%] flex flex-col items-center justify-center bg-white py-[10%] space-y-6 sm:space-y-12 rounded-2xl">
         <div className="flex justify-center text-4xl font-bold text-blue-600">
           Expenser
         </div>
-        <div className="w-full flex flex-col items-center">
-          <div className="w-[80%] flex flex-col space-y-4">
-            <div className="space-y-2">
-              <input
-                type="text"
-                className="w-full px-3 py-2 border-2 rounded-lg"
-                placeholder="Username"
-              />
-              <input
-                type="email"
-                className="w-full px-3 py-2 border-2 rounded-lg"
-                placeholder="Email address"
-              />
-              <input
-                type="password"
-                className="w-full px-3 py-2 border-2 rounded-lg"
-                placeholder="Password"
-              />
-              <input
-                type="password"
-                className="w-full px-3 py-2 border-2 rounded-lg"
-                placeholder="Confirm password"
-              />
-            </div>
-            <div className="w-full bg-blue-600 flex justify-center py-2 rounded-lg text-white text-xl">
-              Sign up
-            </div>
-            <div className="text-lg flex justify-center space-x-1">
-              <span>Already have an account?</span>
-              <Link to={'/login'} className="font-bold text-green-600">Login </Link>
+        <div className="w-full flex flex-col items-center sm:flex-row">
+          <div className="w-full flex flex-col items-center pb-4">
+            <div className="w-[80%] flex flex-col space-y-4">
+              <div className="space-y-1">
+                <input
+                  type="text"
+                  className="w-full px-3 py-2 border-2 rounded-lg"
+                  placeholder="Username"
+                />
+                <input
+                  type="email"
+                  className="w-full px-3 py-2 border-2 rounded-lg"
+                  placeholder="Email address"
+                />
+                <input
+                  type="password"
+                  className="w-full px-3 py-2 border-2 rounded-lg"
+                  placeholder="Password"
+                />
+                <input
+                  type="password"
+                  className="w-full px-3 py-2 border-2 rounded-lg"
+                  placeholder="Confirm password"
+                />
+              </div>
+              <div className="w-full bg-blue-600 flex justify-center py-2 rounded-lg text-white text-xl">
+                Login
+              </div>
+              <div className="text-lg flex justify-center space-x-1">
+                <span>Already have an account?</span>
+                <Link to={"/login"} className="font-bold text-green-600">
+                  Login{" "}
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
 
-        <hr className="w-[90%] border-t-2 border-gray-300" />
+          <div className="w-[90%] h-1 sm:w-1 sm:h-full bg-gray-500 rounded-full"></div>
 
-        <div className="w-full flex flex-col items-center space-y-3">
-          <div className="text-xl">Or join with</div>
-          <div className="flex justify-center w-[80%] bg-red-500 py-2 text-[whitesmoke] text-xl rounded-lg">
-            Google
-          </div>
-          <div className="flex justify-center w-[80%] bg-blue-500 py-2 text-[whitesmoke] text-xl rounded-lg">
-            Facebook
-          </div>
-          <div className="flex justify-center w-[80%] bg-black py-2 text-[whitesmoke] text-xl rounded-lg">
-            Github
+          <div className="w-full flex flex-col items-center space-y-4 mt-4 sm:mt-0">
+            <div className="text-xl">Or continue with</div>
+            <div className="flex justify-center w-[80%] bg-red-500 py-2 text-[whitesmoke] text-xl rounded-lg">
+              Google
+            </div>
+            <div className="flex justify-center w-[80%] bg-blue-500 py-2 text-[whitesmoke] text-xl rounded-lg">
+              Facebook
+            </div>
+            <div className="flex justify-center w-[80%] bg-black py-2 text-[whitesmoke] text-xl rounded-lg">
+              Github
+            </div>
           </div>
         </div>
       </div>
@@ -61,3 +65,4 @@ function Signup() {
 }
 
 export default Signup;
+
