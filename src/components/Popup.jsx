d94bf87a94add78dd4144b5600e6ef75d576b37f
Mohit Yadav <my701319@gmail.com>
2023-11-22 23:55:50 +0530

    nintieth commit

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -13,7 +13,7 @@ function Popup({ showPopup, setShowPopup }) {
       <div
         className={
           onMobile()
-            ? "bg-gray-300 w-[90%] h-[60%] p-[5%] pt-[10%] sm:pt-[15%] shadow-lg shadow-gray-600 flex flex-col justify-between items-center rounded relative sm:w-full sm:h-fit sm:space-y-2"
+            ? "bg-gray-300 w-[90%] h-[60%] p-[5%] pt-[10%] sm:pt-[15%] shadow-lg shadow-gray-600 flex flex-col justify-between items-center rounded relative sm:w-full sm:h-fit sm:space-y-2 sm:overflow-auto"
             : "bg-gray-300 w-[80%] sm:w-[60%] md:w-[50%] lg:w-[40%] xl:w-[30%] 2xl:w-[20%] h-[70%] px-12 pt-12 pb-6 shadow-lg shadow-gray-600 flex flex-col justify-between items-center cursor-pointer rounded relative"
         }
       >
