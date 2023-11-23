a8e9479be9134538b37587c13e6edf82609bd34a
Mohit Yadav <my701319@gmail.com>
2023-11-23 18:22:16 +0530

    hundred-four commit

--- src/components/TotalExpense.jsx
+++ src/components/TotalExpense.jsx
@@ -10,13 +10,25 @@ function TotalExpense({ email, total, setTotal }) {
   return (
     <>
       {total && (
-        <div className="w-full grow flex flex-col">
-          <div className="w-full flex justify-between items-center bg-blue-500 text-white px-4 py-2">
+        <div className="w-full grow flex flex-col overflow-auto relative">
+          <div className="w-full flex justify-between items-center bg-blue-500 text-white px-4 py-2 sticky top-0">
             <div className="flex justify-center items-center">
               <div>Amount &nbsp; = &nbsp;</div>
               <div className="fas fa-inr text-base tracking-wider">{total}</div>
             </div>
             <div className="fas fa-sort"></div>
+          </div>
+          <div>
+            <div>one</div>
+            <div>two</div>
+            <div>three</div>
+            <div>four</div>
+            <div>five</div>
+            <div>six</div>
+            <div>seven</div>
+            <div>eight</div>
+            <div>nine</div>
+            <div>ten</div>
           </div>
         </div>
       )}
